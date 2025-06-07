ea372f5398fbc973cf0b7ff78f152f6a636602e9
janikstucki <janik.stucki@outlook.com>
2025-06-07 15:14:44 +0200

    Refactor email and password editing in Settings view; enhance UI with new button styles and improve layout for better usability

--- AI-Hub/src/views/Settings.vue
+++ AI-Hub/src/views/Settings.vue
@@ -54,23 +54,34 @@ function handleCreateChat() {
       <div class="form-group">
         <label for="email">E-Mail</label>
         <div class="editable-field">
-          <input v-if="isEditingEmail" type="text" id="email" v-model="email" @blur="isEditingEmail = false" />
-          <span v-else>{{ email }}</span>
-          <button class="icon-button" @click="isEditingEmail = true">
-            ✏️
+          <div class="field-content">
+            <input v-if="isEditingEmail" type="text" id="email" v-model="email" @blur="isEditingEmail = false" />
+            <span v-else>{{ email }}</span>
+          </div>
+          <button class="icon-button" @click="EditChat(chat)">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+              stroke-width="1.5" stroke="currentColor" class="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round"
+                d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+            </svg>
           </button>
         </div>
 
         <label for="password">Passwort</label>
         <div class="editable-field">
-          <input v-if="isEditingPassword" type="text" id="password" v-model="password" @blur="isEditingPassword = false" />
-          <span v-else>{{ password }}</span>
-          <button class="icon-button" @click="isEditingPassword = true">
-            ✏️
+          <div class="field-content">
+            <input v-if="isEditingPassword" type="text" id="password" v-model="password" @blur="isEditingPassword = false" />
+            <span v-else>{{ password }}</span>
+          </div>
+          <button class="icon-button" @click="EditChat(chat)">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+              stroke-width="1.5" stroke="currentColor" class="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round"
+                d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+            </svg>
           </button>
         </div>
       </div>
-
       <div v-if="models.length === 0">
         <p>Noch kein Model vorhanden.</p>
       </div>
@@ -91,12 +102,18 @@ function handleCreateChat() {
               class="chat-title-input"
             />
             <div class="chat-actions">
-              <button class="icon-button" @click="chat.isEditing = true">
-                ✏️
-              </button>
-              <button class="icon-button" @click="DeleteModel(chat)">
-                🗑️
-              </button>
+              <button class="icon-button" @click="EditChat(chat)">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                    </svg>
+
+                </button>
+              <button class="icon-button" @click="DeleteChat(chat)">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                    </svg>
+
+                </button>
             </div>
           </div>
         </a>
@@ -143,6 +160,7 @@ function handleCreateChat() {
   align-items: center;
   z-index: 1000;
 }
+
 .icon-button {
   background: none;
   border: none;
@@ -232,6 +250,25 @@ input {
   box-sizing: border-box;
 }
 
+.editable-field {
+  position: relative;
+  display: flex;
+  align-items: center;
+}
+
+.editable-field input,
+.editable-field span {
+  flex: 1;
+  padding-right: 36px; /* Platz für den Button */
+}
+
+.editable-field .icon-button {
+  position: absolute;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
 .chat-row {
   display: flex;
   justify-content: space-between;
@@ -265,7 +302,6 @@ input {
 .chat-link:hover {
   background-color: #e0e0e0; 
 }
-
 
 #nwButton {
   width: 100%;
