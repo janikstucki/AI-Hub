8c01097a619a4d51c41e2e118b04f23cd3a315cf
janikstucki <janik.stucki@outlook.com>
2025-06-10 19:54:48 +0200

    Add back button to Login, NewChat, Register, and Settings views with updated styles

--- AI-Hub/src/views/NewChat.vue
+++ AI-Hub/src/views/NewChat.vue
@@ -22,6 +22,12 @@ function NewChat ()  {
 
 <template>
   <div id="nwcontainer">
+    <button class="back-button" @click="router.push('/')">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
+</svg>
+
+    </button>
     <div class="nwview">
       <h1>Neuer Chat</h1>
       <form @submit.prevent="handleCreateChat">
@@ -58,6 +64,30 @@ body {
   margin: 0;
   padding: 0;
   height: 100%;
+}
+
+.back-button{
+
+  background: none;
+  border: none;
+  cursor: pointer;
+  padding: 6px;
+  border-radius: 6px;
+  color: inherit;
+  transition: color 0.2s ease;
+  position: fixed;
+  top: 35%;
+  left: 40%;
+}
+
+.back-button:hover svg {
+  stroke: #4d6bfe; 
+}
+
+
+.back-button svg {
+  width: 24px;
+  height: 24px;
 }
 
 input {
