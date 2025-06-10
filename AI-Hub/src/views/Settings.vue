eee66bdc6552f5304dce3b65c3e1295e1dab9968
janikstucki <janik.stucki@outlook.com>
2025-06-10 19:45:44 +0200

    Refactor Settings.vue to remove model name handling; update chat display to show API keys instead

--- AI-Hub/src/views/Settings.vue
+++ AI-Hub/src/views/Settings.vue
@@ -9,20 +9,15 @@ const isEditingEmail = ref(false);
 const isEditingPassword = ref(false);
 
 const showModal = ref(false);
-const newModelName = ref("");
 const newApiKey = ref("");
 
 const models = ref([
-  { id: 1, title: "Chat-GPT", apiKey: "1231321" },
-  { id: 2, title: "Claude", apiKey: "fgfgf" },
+  { id: 1, apiKey: "1231321" },
+  { id: 2, apiKey: "fgfgf" },
 ]);
 
-function EditChat(chat) {
-  chat.isEditing = true;
-}
-
 function SaveModelTitle(chat, event) {
-  chat.title = event.target.value;
+  chat.apiKey = event.target.value;
   chat.isEditing = false;
 }
 
@@ -34,20 +29,19 @@ function DeleteChat(chat) {
 }
 
 function handleCreateChat() {
-  if (!newModelName.value || !newApiKey.value) return;
+  if (!newApiKey.value) return;
 
   const newId = models.value.length + 1;
   models.value.push({
     id: newId,
-    title: newModelName.value,
-    model: "custom",
+    apiKey: newApiKey.value, 
     isEditing: false,
   });
 
-  newModelName.value = "";
   newApiKey.value = "";
   showModal.value = false;
 }
+
 </script>
 
 <template>
@@ -59,32 +53,24 @@ function handleCreateChat() {
         <label for="email">E-Mail</label>
         <div class="editable-field">
           <div class="field-content">
-            <input v-if="isEditingEmail" type="text" id="email" v-model="email" @blur="isEditingEmail = false" @keyup.enter="isEditingEmail = false" />
-            <span v-else>{{ email }}</span>
+                        <div>{{ email }}</div>
+
           </div>
-          <button class="icon-button" @click="isEditingEmail = true">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-              <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-            </svg>
-          </button>
+         
         </div>
 
         <label for="password">Passwort</label>
         <div class="editable-field">
           <div class="field-content">
-            <input v-if="isEditingPassword" type="text" id="password" v-model="password" @blur="isEditingPassword = false" @keyup.enter="isEditingPassword = false" />
-            <span v-else>{{ password }}</span>
+
+            <div>{{ password }}</div>
           </div>
-          <button class="icon-button" @click="isEditingPassword = true">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-              <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-            </svg>
-          </button>
+          
         </div>
       </div>
 
       <div v-if="models.length === 0">
-        <p>Noch kein Model vorhanden.</p>
+        <p>Noch kein API-Key vorhanden.</p>
       </div>
 
       <div v-else>
@@ -95,19 +81,15 @@ function handleCreateChat() {
           class="chat-link"
         >
           <div class="chat-row">
-            <span v-if="!chat.isEditing">{{ chat.title }}</span>
+            <span v-if="!chat.isEditing">{{ chat.apiKey }}</span>
             <input
               v-else
-              v-model="chat.title"
+              v-model="chat.apiKey"
               @keyup.enter="SaveModelTitle(chat, $event)"
               class="chat-title-input"
             />
             <div class="chat-actions">
-              <button class="icon-button" @click="EditChat(chat)">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                </svg>
-              </button>
+              
               <button class="icon-button" @click="DeleteChat(chat)">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                   <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
@@ -118,15 +100,12 @@ function handleCreateChat() {
         </a>
       </div>
 
-      <button id="nwButton" @click="showModal = true">Neues Model hinzufügen</button>
+      <button id="nwButton" @click="showModal = true">API-Key hinzufügen</button>
       <button id="Button" type="button" @click="router.push('/')">Zurück</button>
 
       <div v-if="showModal" class="modal-overlay" @click.self="showModal = false">
         <div class="modal">
-          <h3>Neues Model erstellen</h3>
           <form @submit.prevent="handleCreateChat">
-            <label for="model-name">Model-Name:</label>
-            <input type="text" id="model-name" v-model="newModelName" />
             <label for="api-key">API-Key:</label>
             <input type="text" id="api-key" v-model="newApiKey" />
             <button id="nwButton" type="submit">Erstellen</button>
